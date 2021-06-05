1a7edb996d05ca15c2d16477b99c009f99abe818
ANearchou <andreasnearchou9@gmail.com>
2021-06-06 00:16:49 +0300

    simple moving average function

---
+++ src/moving_avgs.cpp
@@ -0,0 +1,38 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+NumericVector sma(NumericVector x, int n){
+
+    int m = x.length();
+    NumericVector ma(m-n+1);
+    for (int i = 0; i<m-n+1; ++i ){
+        double s = 0;
+        for (int j = i; j<i+n; ++j){
+            s = s + x[j];
+        }
+        ma[i] = s/n;
+    }
+    return(ma);
+    
+}
+
+
+
+// [[Rcpp::export]]
+NumericVector simple_moving_avg(DataFrame dt, int n){
+    
+    NumericVector x = dt["x"];
+    int m = x.length();
+    NumericVector ma(m);
+    for (int i = 0; i<m-n+1; ++i ){
+        double s = 0;
+        for (int j = i; j<i+n; ++j){
+            s = s + x[j];
+        }
+        ma[i] = s/n;
+    }
+    return(ma);
+    
+}
